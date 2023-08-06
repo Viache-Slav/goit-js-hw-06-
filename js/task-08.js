6fa2654cf0a4ee616da1c31a7cf7c3ffad2a9733
Viache-Slav <slavikfedko@gmail.com>
2023-08-06 19:05:59 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const inputEl = document.querySelector('.login-form')
 
+inputEl.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const {
+        elements: { email, password },
+    } = event.currentTarget
+
+    if (email.value === '' || password.value === '') {
+        return alert("proszę wypełnić wszystkie pola")
+    }
+    const userDetails = { email: email.value, Password: password.value }
+
+console.log(userDetails)
+event.currentTarget.reset()
+}
